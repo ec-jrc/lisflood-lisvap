2078334ac375c61114470c3d1071f12d6518b321
valeriolorini <39339404+valeriolorini@users.noreply.github.com>
2018-08-06 15:27:45 +0200

    1st commirt

---
+++ hydrological_modules/readmeteo.py
@@ -0,0 +1,115 @@
+# -------------------------------------------------------------------------
+# Name:        READ METEO input maps
+# Purpose:
+#
+# Author:      burekpe
+#
+# Created:     12/08/2014
+# Copyright:   (c) burekpe 2014
+# Licence:     <your licence>
+# -------------------------------------------------------------------------
+
+from global_modules.add1 import *
+
+class readmeteo(object):
+
+    """
+     # ************************************************************
+     # ***** READ METEOROLOGICAL DATA              ****************
+     # ************************************************************
+    """
+
+    def __init__(self, readmeteo_variable):
+        self.var = readmeteo_variable
+
+# --------------------------------------------------------------------------
+# --------------------------------------------------------------------------
+
+    def dynamic(self):
+        """ dynamic part of the readmeteo module
+            read meteo input maps
+        """
+
+        # ************************************************************
+        # ***** READ METEOROLOGICAL DATA *****************************
+        # ************************************************************
+        if option['readNetcdfStack']:
+            if option['CORDEX']:
+                self.var.TAvg = readnetcdf(binding['TAvgMaps'], self.var.currentTimeStep())
+                self.var.TMin = readnetcdf(binding['TMinMaps'], self.var.currentTimeStep())
+                self.var.TMax = readnetcdf(binding['TMaxMaps'], self.var.currentTimeStep())
+
+                self.var.Psurf = readnetcdf(binding['PSurfMaps'], self.var.currentTimeStep())
+                self.var.Qair = readnetcdf(binding['QAirMaps'], self.var.currentTimeStep())
+                self.var.Wind = readnetcdf(binding['WindMaps'], self.var.currentTimeStep())
+
+                self.var.Rds = readnetcdf(binding['RdsMaps'], self.var.currentTimeStep())
+                # Downward  short wave radiation [W/m2]
+                self.var.Rdl = readnetcdf(binding['RdlMaps'], self.var.currentTimeStep())
+                # Down long wave radiation [W/m2]
+                self.var.Rus = readnetcdf(binding['RusMaps'], self.var.currentTimeStep())
+                # upward  short wave radiation [W/m2]
+                self.var.Rul = readnetcdf(binding['RulMaps'], self.var.currentTimeStep())
+                # upward long wave radiation [W/m2]
+            if option['EFAS']:
+                self.var.TMin = readnetcdf(binding['TMinMaps'], self.var.currentTimeStep())
+                # Minimum daily temperature (C)
+                self.var.TMax = readnetcdf(binding['TMaxMaps'], self.var.currentTimeStep())
+                # Maximum daily temperature (C)
+                self.var.TAvg = (self.var.TMin + self.var.TMax) / 2.0
+                # Average daily temperature (C)
+
+                self.var.EAct = readnetcdf(binding['EActMaps'], self.var.currentTimeStep())
+                # actual vapor pressure; has to be in mbar = hPa
+                # self.var.EAct = self.var.EAct / 10
+                # from hPa tp kPa
+                # actual vapour pressure (pd maps): typical value 0-70 hPa = 0-7 kPa
+
+                self.var.Wind = readnetcdf(binding['WindMaps'], self.var.currentTimeStep())
+                # near surface windspeed at 10 m
+                self.var.Wind = self.var.Wind * 0.749
+                # Adjust wind speed for measurement height: wind speed measured at
+                # 10 m, but needed at 2 m height
+                # Shuttleworth, W.J. (1993) in Maidment, D.R. (1993), p. 4.36
+                # Typical input values 0-15 m/s (wind at 10m)
+
+                self.var.Rgd = readnetcdf(binding['RgdMaps'], self.var.currentTimeStep())
+                # calculated radiation [J/m2/day]
+                # Incoming (downward surface) solar radiation [J/m2/d] (SSRD variable in ERA40)
+                # typical vale: 29410560 J/m2/day = 340.4 W/m2 (1 W = 1 J/s)
+
+
+
+        if self.var.TemperatureInKelvinFlag==1:
+            self.var.TAvg = self.var.TAvg - self.var.ZeroKelvin
+            self.var.TMin = self.var.TMin - self.var.ZeroKelvin
+            self.var.TMax = self.var.TMax - self.var.ZeroKelvin
+
+        self.var.DeltaT=pcraster.max(self.var.TMax-self.var.TMin,scalar(0.0))
+
+
+        if option['CORDEX']:
+            # correction from sea level pressure to pressure at elevation of dem
+            a = 16000+64*self.var.TAvg
+            self.var.Psurf = self.var.Psurf*(a-self.var.dem)/(a+self.var.dem)
+            # and back to the high reso dem with adjusted temp
+            self.var.Rds = self.var.Rds * self.var.WtoMJ
+            self.var.Rdl = self.var.Rdl * self.var.WtoMJ
+            self.var.Rus = self.var.Rus * self.var.WtoMJ
+            self.var.Rul = self.var.Rul * self.var.WtoMJ
+
+
+        self.var.Psurf = self.var.Psurf * 0.001
+            # [Pa] to [KPa]
+
+
+            self.var.EAct = (self.var.Psurf * self.var.Qair)/0.622
+             # [KPA] * [kg/kg] = KPa
+        self.var.Wind = self.var.Wind * 0.749
+            # Adjust wind speed for measurement height: wind speed measured at
+            # 10 m, but needed at 2 m height
+            # Shuttleworth, W.J. (1993) in Maidment, D.R. (1993), p. 4.36
+        
+        
+
+
---
+++ lisvap1.py
@@ -0,0 +1,133 @@
+#  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+"""
+#######################################################
+
+  ##       ####  ######  ##     ##    ###    ########
+  ##        ##  ##    ## ##     ##   ## ##   ##     ##
+  ##        ##  ##       ##     ##  ##   ##  ##     ##
+  ##        ##   ######  ##     ## ##     ## ########
+  ##        ##        ##  ##   ##  ######### ##
+  ##        ##  ##    ##   ## ##   ##     ## ##
+  ######## ####  ######     ###    ##     ## ##
+
+#######################################################
+"""
+
+__authors__ = "Peter Burek, Johan van der Knijff, Ad de Roo"
+__version__ = "Version: 0.02"
+__date__ ="12/09/2014"
+__copyright__ = "Copyright 2014, The LisfloodPy Project"
+__maintainer__ = "Peter Burek"
+__status__ = "Development"
+
+
+#  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+from Lisvap_initial import *
+from Lisvap_dynamic import *
+
+class LisvapModel(LisvapModel_ini, LisvapModel_dyn):
+    """ Joining the initial and the dynamic part
+        of the Lisflood model
+    """
+
+
+# ==================================================
+# ============== LISFLOOD execute ==================
+# ==================================================
+
+def Lisvapexe():
+
+    optionBinding(settings, optionxml)
+    # read all the possible option for modelling and for generating output
+    # read the settingsfile with all information about the catchments(s)
+    # and the choosen option for mdelling and output
+    bindkey = sorted(binding.keys())
+
+    """
+    for key in bindkey:
+        try: x = float(binding[key])
+        except: s = '\'' + binding[key] + '\''
+    """
+
+    StepStart = int(binding['StepStart'])
+    StepEnd = int(binding['StepEnd'])
+    print "Start - End: ",StepStart," - ", StepEnd
+    if Flags['loud']:
+        print"%-6s %10s %11s\n" %("Step","Date","ET0"),
+
+    Lisvap = LisvapModel()
+    stLisvap = DynamicFrame(Lisvap, firstTimestep=StepStart, lastTimeStep=StepEnd)
+    stLisvap.rquiet = True
+    stLisvap.rtrace = False
+    stLisvap.run()
+    # cProfile.run('stLisflood.run()')
+    # python -m cProfile -o  l1.pstats lisf1.py settingsNew3.xml
+    # gprof2dot -f pstats l1.pstats | dot -Tpng -o callgraph.png
+
+    if Flags['printtime']:
+        print "\n\nTime profiling"
+        print "%2s %-17s %10s %8s" %("No","Name","time[s]","%")
+        for i in xrange(len(timeMesSum)):
+            print "%2i %-17s %10.2f %8.1f"  %(i,timeMesString[i],timeMesSum[i],100 * timeMesSum[i] / timeMesSum[-1])
+
+# ==================================================
+# ============== USAGE ==============================
+# ==================================================
+
+
+def usage():
+    """ prints some lines describing how to use this program
+        which arguments and parameters it accepts, etc
+    """
+    print 'LisvapPy - Lisvap (Global) using pcraster Python framework'
+    print 'Authors: ', __authors__
+    print 'Version: ', __version__
+    print 'Date: ', __date__
+    print 'Status: ', __status__
+    print """
+    Arguments list:
+    settings.xml     settings file
+
+    -q --quiet       output progression given as .
+    -v --veryquiet   no output progression is given
+    -l --loud        output progression given as time step, date and discharge
+    -c --check       input maps and stack maps are checked, output for each input map BUT no model run
+    -h --noheader    .tss file have no header and start immediately with the time series
+    -t --printtime   the computation time for hydrological modules are printed
+    """
+    sys.exit(1)
+
+def headerinfo():
+
+   print "Lisvap ",__version__," ",__date__,
+   print """
+Water balance and flood simulation model for large catchments\n
+(C) Institute for Environment and Sustainability
+    Joint Research Centre of the European Commission
+    TP122, I-21020 Ispra (Va), Italy\n"""
+
+
+# ==================================================
+# ============== MAIN ==============================
+# ==================================================
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 2:
+        usage()
+
+    LF_Path = os.path.dirname(sys.argv[0])
+    LF_Path = os.path.abspath(LF_Path)
+    optionxml = os.path.normpath(LF_Path + "/OptionTserieMapsLisvap.xml")
+
+    settings = sys.argv[1]    # setting.xml file
+
+    args = sys.argv[2:]
+    globalFlags(args)
+    # setting of global flag e.g checking input maps, producing more output information
+    if not(Flags['veryquiet']) and not(Flags['quiet']) : headerinfo()
+    Lisvapexe()
