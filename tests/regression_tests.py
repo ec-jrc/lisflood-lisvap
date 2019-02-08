929019193e0cb3380b5efe9913493ae9fd1b9cd7
Domenico Nappo <domenico.nappo@gmail.com>
2019-02-08 15:19:29 +0100

    tests for et and es, including forcing maps in test fixtures (compressed, sliced from 1 to 9)

--- tests/regression_tests.py
+++ tests/regression_tests.py
@@ -5,45 +5,46 @@ from pcraster.numpy_operations import pcr2numpy
 
 from global_modules.add1lisvap import readnetcdf
 from global_modules.globals import binding
-from tests import reference_nc_path, atol
+from tests import reference_nc_paths, atol
 
 
 def test_output():
     print '\n'
     output_path = binding['PathOut']
-    output_e0 = os.path.join(output_path, 'e0.nc')
     results = []
-    for step in xrange(1, 10):
-        e0_reference = pcr2numpy(readnetcdf(reference_nc_path, step, value='e0'), -9999)
-        e0_out = pcr2numpy(readnetcdf(output_e0, step, value='e0'), -9999)
-        same_size = e0_reference.size == e0_out.size
-        diff_values = np.abs(e0_reference - e0_out)
-        same_values = np.allclose(diff_values, np.zeros(diff_values.shape), atol=atol)
-        all_ok = same_size and same_values
-        if not all_ok:
-            array_ok = np.isclose(diff_values, np.zeros(diff_values.shape), atol=atol)
-            wrong_values_size = array_ok[array_ok is False].size
-            if wrong_values_size > 0:
-                max_diff = np.max(diff_values)
-                large_diff = max_diff > 2 * 0.01
-                perc_wrong = float(wrong_values_size * 100) / float(diff_values.size)
-                print 'Step {} ---> Max Diff: {:3.9f}, % Wrong values: {:3.9f} ({})'.format(step, max_diff, perc_wrong, wrong_values_size)
-                if perc_wrong >= 0.05:
-                    print '[ERROR]'
-                    print 'STEP {}: {:3.9f}% of values are different. max diff: {:3.4f}'.format(step, perc_wrong, max_diff)
-                    results.append(False)
-                elif perc_wrong >= 0.0001 and large_diff:
-                    print '[WARNING]'
-                    print 'STEP {}: {:3.9f}% of values have large difference. max diff: {:3.4f}'.format(step, perc_wrong, max_diff)
-                    results.append(False)
+    for var in reference_nc_paths:
+        output_nc = os.path.join(output_path, '{}.nc'.format(var))
+        for step in xrange(1, 10):
+            reference = pcr2numpy(readnetcdf(reference_nc_paths[var], step, value=var), -9999)
+            current_output = pcr2numpy(readnetcdf(output_nc, step, value=var), -9999)
+            same_size = reference.size == current_output.size
+            diff_values = np.abs(reference - current_output)
+            same_values = np.allclose(diff_values, np.zeros(diff_values.shape), atol=atol)
+            all_ok = same_size and same_values
+            if not all_ok:
+                array_ok = np.isclose(diff_values, np.zeros(diff_values.shape), atol=atol)
+                wrong_values_size = array_ok[array_ok is False].size
+                if wrong_values_size > 0:
+                    max_diff = np.max(diff_values)
+                    large_diff = max_diff > 2 * 0.01
+                    perc_wrong = float(wrong_values_size * 100) / float(diff_values.size)
+                    print 'Var: {} - Step {} ---> Max Diff: {:3.9f}, % Wrong values: {:3.9f} ({})'.format(var, step, max_diff, perc_wrong, wrong_values_size)
+                    if perc_wrong >= 0.05:
+                        print '[ERROR]'
+                        print 'Var: {} - STEP {}: {:3.9f}% of values are different. max diff: {:3.4f}'.format(var, step, perc_wrong, max_diff)
+                        results.append(False)
+                    elif perc_wrong >= 0.0001 and large_diff:
+                        print '[WARNING]'
+                        print 'Var: {} - STEP {}: {:3.9f}% of values have large difference. max diff: {:3.4f}'.format(var, step, perc_wrong, max_diff)
+                        results.append(False)
+                    else:
+                        print 'Var: {} - [OK] Step: {}'.format(var, step)
+                        results.append(True)
                 else:
-                    print '[OK] Step: {}'.format(step)
+                    print 'Var: {} - [OK] Step: {}'.format(var, step)
                     results.append(True)
             else:
-                print '[OK] Step: {}'.format(step)
+                print 'Var: {} - [OK] Step: {}'.format(var, step)
                 results.append(True)
-        else:
-            print '[OK] Step: {}'.format(step)
-            results.append(True)
 
     assert all(results)
