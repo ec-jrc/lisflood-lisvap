f77680ce6cfc5e1145b6e42b2bda4eacfa9b5655
Domenico Nappo <domenico.nappo@gmail.com>
2019-04-19 12:51:53 +0200

    rel 0.3.2

--- src/lisvap/lisvap1.py
+++ src/lisvap/lisvap1.py
@@ -28,7 +28,7 @@ See the Licence for the specific language governing permissions and limitations 
 """
 
 __authors__ = "Peter Burek, Johan van der Knijff, Ad de Roo"
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 __date__ = "19/04/2019"
 __copyright__ = "Copyright 2019, Lisflood Open Source"
 __maintainers__ = "Domenico Nappo, Valerio Lorini, Lorenzo Mentaschi"
