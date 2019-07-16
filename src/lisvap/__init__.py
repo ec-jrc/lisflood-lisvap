2aef446ee74c6c60aa03e80d9cdfe8bb97d400e0
Domenico Nappo <domenico.nappo@gmail.com>
2019-07-16 09:55:54 +0200

    fix bad merge #2

--- src/lisvap/__init__.py
+++ src/lisvap/__init__.py
@@ -4,3 +4,4 @@ __version__ = '.'.join(list(map(str, version)))
 __date__ = "15/07/2019"
 __copyright__ = "Copyright 2019, Lisflood Open Source"
 __maintainers__ = "Domenico Nappo, Valerio Lorini, Lorenzo Mentaschi, Lorenzo Alfieri"
+__status__ = "Development"
