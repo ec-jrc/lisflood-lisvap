310a3197d59e753451eecc1157bb2417329800ee
jrgg <goncalo.ramos-gomes@ext.ec.europa.eu>
2021-03-10 19:58:36 +0100

    Create test case for yearly IO. Code review on the write netcdf.

--- src/lisvap/utils/writers.py
+++ src/lisvap/utils/writers.py
@@ -29,6 +29,7 @@ from pcraster import numpy_operations
 from decimal import *
 
 from . import CutMap, NetcdfMetadata, LisSettings
+from pyproj import proj
 # from scprep.run.splatter import SplatSimulate
 # from audioop import reverse
 
@@ -61,31 +62,11 @@ def coordinates_range(start=0, nelems=1, step=1):
     return elem_array
 
 
-def writenet(current_output_index, inputmap, netfile, current_timestep, value_standard_name, value_long_name,
-             value_unit, data_type, calendar_day_start, flag_time=True, nan_value=-9999, scale_factor=0.1,
-             add_offset=0.0, value_min=0, value_max=-9999):
-    """
-    write a netcdf stack
-    output_index: integer. Global index of the map to store in the final file
-    inputmap: a PCRaster 2D array
-    netfile: output netcdf filename
-    timestep:
-    """
-    start_date = calendar_day_start + datetime.timedelta(days=1)
-    timestep = current_timestep
+def get_output_parameters(settings, netcdf_output_file, start_date, timestep, current_output_index):
     output_index = current_output_index
-    p = Path(netfile)
+    p = Path(netcdf_output_file)
     netfile = Path(p.parent) / Path('{}.nc'.format(p.name) if not p.name.endswith('.nc') else p.name)
     prefix = os.path.splitext(netfile.name)[0]
-
-    cutmap = CutMap.instance()
-    nrows = np.abs(cutmap.cuts[3] - cutmap.cuts[2])
-    ncols = np.abs(cutmap.cuts[1] - cutmap.cuts[0])
-
-    settings = LisSettings.instance()
-
-    time_variable = 'time'
-    output6hourly = settings.options['output6hourly']
     splitIO = settings.options['splitOutput']
     if splitIO:
         start_year = start_date.strftime('%Y')
@@ -110,113 +91,173 @@ def writenet(current_output_index, inputmap, netfile, current_timestep, value_st
                 output_index = days_between - 1
             # output_index = days_between
         netfile = Path(p.parent) / Path('{}_{}.nc'.format(p.name, filename_year) if not p.name.endswith('.nc') else p.name)
-    # Used to pack variables into short
+
+    return prefix, netfile, output_index
+
+
+def set_time_dimension(netcdf_obj, time_variable_name, start_date, variable_dims, output6hourly=False):
+    netcdf_obj.createDimension(time_variable_name, None)
+    dims = list(variable_dims)
+    dims.append(time_variable_name)
+    dims.reverse()
+    variable_dims = tuple(dims)
+    time = netcdf_obj.createVariable(time_variable_name, 'f4', (time_variable_name, ))
+    time.standard_name = time_variable_name
+    if output6hourly:
+        time.units = 'hours since %s' % start_date.strftime('%Y-%m-%d %H:%M:%S.0')
+        time.frequency = 6
+    else:
+        time.units = 'days since %s' % start_date.strftime('%Y-%m-%d %H:%M:%S.0')
+        time.frequency = 1
+    time.calendar = 'proleptic_gregorian'
+    return variable_dims
+
+
+def set_dimensions(netcdf_obj, metadata_ncdf, ncols, nrows, time_variable_name, start_date,
+                   output6hourly=False, flag_time=True):
+    # Dimension
+    spatial_dims = tuple([c for c in ('x', 'lon', 'y', 'lat') if c in metadata_ncdf])
+    for dim_name, dim_size in zip(spatial_dims, [ncols, nrows]):
+        netcdf_obj.createDimension(dim_name, dim_size)
+
+    variable_dims = spatial_dims
+    if flag_time:
+        variable_dims = set_time_dimension(netcdf_obj, time_variable_name, start_date, variable_dims, output6hourly)
+
+    reverse_spatial_dims = list(spatial_dims)
+    reverse_spatial_dims.reverse()
+    for dim_name in reverse_spatial_dims:
+        coord = netcdf_obj.createVariable(dim_name, 'f8', (dim_name, ))
+        for i in metadata_ncdf[dim_name]:
+            # to avoid AttributeError ("_FillValue attribute must be set when variable is created") when writing output nc attributes
+            if i != '_FillValue':
+                setattr(coord, i, metadata_ncdf[dim_name][i])
+    return spatial_dims, variable_dims
+
+
+def set_projection(netcdf_obj, metadata_ncdf):
+    # projection
+    proj = None
+    metadata_ncdf_projections = {
+        'laea': 'laea',
+        'lambert_azimuthal_equal_area': 'laea',
+        'wgs_1984': 'wgs_1984',
+        'crs': 'crs'
+    }
+    selected_proj_key = ''
+    for proj_key in metadata_ncdf_projections:
+        if proj_key in metadata_ncdf:
+            selected_proj_key = proj_key
+            variable_name = metadata_ncdf_projections[proj_key]
+            proj = netcdf_obj.createVariable(variable_name, 'i4')
+            # Copy all other attributes
+            for i in metadata_ncdf[proj_key]:
+                setattr(proj, i, metadata_ncdf[proj_key][i])
+    return proj, selected_proj_key
+
+
+def get_coordinate_arrays(settings, ncols, nrows):
+    # Fill variables
+    try:
+        int_lons = settings.binding['internal.lons']
+        int_lats = settings.binding['internal.lats']
+        # x1 = Decimal(__DECIMAL_FORMAT.format(int_lons[0]))
+        # x2 = Decimal(__DECIMAL_FORMAT.format(int_lons[1]))
+        # y1 = Decimal(__DECIMAL_FORMAT.format(int_lats[0]))
+        # y2 = Decimal(__DECIMAL_FORMAT.format(int_lats[1]))
+        # cellSizeX = abs(x2 - x1)
+        # cellSizeY = abs(y2 - y1)
+        lats = coordinates_range_from_array(int_lats)
+        lons = coordinates_range_from_array(int_lons)
+    except:
+        cellSize = Decimal(__DECIMAL_FORMAT.format(pcraster.clone().cellSize()))
+        half_cell = cellSize * Decimal(0.5)
+        xl = Decimal(__DECIMAL_FORMAT.format(pcraster.clone().west())) + half_cell
+        # xr = xl + ncols * cellSize - half_cell
+        yu = Decimal(__DECIMAL_FORMAT.format(pcraster.clone().north())) - half_cell
+        # yd = yu - nrows * cellSize + half_cell
+        lats = coordinates_range(yu, nrows, -cellSize)
+        lons = coordinates_range(xl, ncols, cellSize)
+    return lats, lons
+
+
+def create_new_netcdf(settings, prefix, netfile, ncols, nrows, time_variable, start_date,
+                      value_standard_name, value_long_name, value_unit, data_type,
+                      scale_factor=0.1, add_offset=0.0, value_min=0, value_max=-9999,
+                      output6hourly=False, flag_time=True, nan_value=-9999):
+    nf1 = Dataset(netfile, 'w', format='NETCDF4_CLASSIC')
+    # general Attributes
+    nf1.history = 'Created ' + xtime.ctime(xtime.time())
+    nf1.Conventions = 'CF-1.6'
+    nf1.Source_Software = 'Lisvap'
+    nf1.source = 'Lisvap output maps'
+
+    metadata_ncdf = NetcdfMetadata.instance()
+
+    spatial_dims, variable_dims = set_dimensions(nf1, metadata_ncdf, ncols, nrows, time_variable,
+                                                 start_date, output6hourly, flag_time)
+
+    proj, selected_proj_key = set_projection(nf1, metadata_ncdf)
+
+    value = nf1.createVariable(prefix, data_type, variable_dims, zlib=True, complevel=4, fill_value=nan_value)
+
+    value.standard_name = value_standard_name
+    value.long_name = value_long_name
+    value.units = value_unit
+    value.valid_min = int(value_min / scale_factor)
+    if value_max != nan_value:
+        value.valid_max = int(value_max / scale_factor)
+    value.scale_factor = scale_factor
+    value.add_offset = add_offset
+    value.missing_value = nan_value
+    value.set_auto_maskandscale(True)
+
+    if proj is not None:
+        if 'grid_mapping_name' in metadata_ncdf[selected_proj_key]:
+            value.grid_mapping = proj.grid_mapping_name
+        if 'spatial_ref' in metadata_ncdf[selected_proj_key]:
+            value.esri_pe_string = proj.spatial_ref
+
+    lats, lons = get_coordinate_arrays(settings, ncols, nrows)
+
+    nf1.variables[spatial_dims[1]][:] = lats
+    nf1.variables[spatial_dims[0]][:] = lons
+
+    if 'pr' in metadata_ncdf and 'esri_pe_string' in metadata_ncdf['pr']:
+        value.esri_pe_string = metadata_ncdf['pr']['esri_pe_string']
+    return nf1
+
+
+def writenet(current_output_index, inputmap, netcdf_output_file, current_timestep, value_standard_name, value_long_name,
+             value_unit, data_type, calendar_day_start, flag_time=True, nan_value=-9999, scale_factor=0.1,
+             add_offset=0.0, value_min=0, value_max=-9999):
+    """
+    write a netcdf stack
+    output_index: integer. Global index of the map to store in the final file
+    inputmap: a PCRaster 2D array
+    netfile: output netcdf filename
+    timestep:
+    """
+    start_date = calendar_day_start + datetime.timedelta(days=1)
+    timestep = current_timestep
+
+    cutmap = CutMap.instance()
+    nrows = np.abs(cutmap.cuts[3] - cutmap.cuts[2])
+    ncols = np.abs(cutmap.cuts[1] - cutmap.cuts[0])
+
+    settings = LisSettings.instance()
+
+    time_variable = 'time'
+    output6hourly = settings.options['output6hourly']
+    prefix, netfile, output_index = get_output_parameters(settings, netcdf_output_file, start_date,
+                                                          timestep, current_output_index)
+
+    # Create and setup the netcdf file when the first map needs to be stored
     if output_index == 0 or not netfile.exists():
-        nf1 = Dataset(netfile, 'w', format='NETCDF4_CLASSIC')
-
-        # general Attributes
-        nf1.history = 'Created ' + xtime.ctime(xtime.time())
-        nf1.Conventions = 'CF-1.6'
-        nf1.Source_Software = 'Lisvap'
-        nf1.source = 'Lisvap output maps'
-
-        metadata_ncdf = NetcdfMetadata.instance()
-
-        # Dimension
-        spatial_dims = tuple([c for c in ('x', 'lon', 'y', 'lat') if c in metadata_ncdf])
-        for dim_name, dim_size in zip(spatial_dims, [ncols, nrows]):
-            nf1.createDimension(dim_name, dim_size)
-
-        variable_dims = spatial_dims
-        if flag_time:
-            nf1.createDimension(time_variable, None)
-            dims = list(variable_dims)
-            dims.append(time_variable)
-            dims.reverse()
-            variable_dims = tuple(dims)
-            time = nf1.createVariable(time_variable, 'f4', (time_variable, ))
-            time.standard_name = time_variable
-            if output6hourly:
-                time.units = 'hours since %s' % start_date.strftime('%Y-%m-%d %H:%M:%S.0')
-                time.frequency = 6
-            else:
-                time.units = 'days since %s' % start_date.strftime('%Y-%m-%d %H:%M:%S.0')
-                time.frequency = 1
-            time.calendar = 'proleptic_gregorian'
-
-        reverse_spatial_dims = list(spatial_dims)
-        reverse_spatial_dims.reverse()
-        for dim_name in reverse_spatial_dims:
-            coord = nf1.createVariable(dim_name, 'f8', (dim_name, ))
-            for i in metadata_ncdf[dim_name]:
-                if i != '_FillValue':
-                    # to avoid AttributeError ("_FillValue attribute must be set when variable is created") when writing output nc attributes
-                    setattr(coord, i, metadata_ncdf[dim_name][i])
-
-        # projection
-        proj = None
-        metadata_ncdf_projections = {
-            'laea': 'laea',
-            'lambert_azimuthal_equal_area': 'laea',
-            'wgs_1984': 'wgs_1984',
-            'crs': 'crs'
-        }
-        selected_proj_key = ''
-        for proj_key in metadata_ncdf_projections:
-            if proj_key in metadata_ncdf:
-                selected_proj_key = proj_key
-                variable_name = metadata_ncdf_projections[proj_key]
-                proj = nf1.createVariable(variable_name, 'i4')
-                # Copy all other attributes
-                for i in metadata_ncdf[proj_key]:
-                    setattr(proj, i, metadata_ncdf[proj_key][i])
-
-        value = nf1.createVariable(prefix, data_type, variable_dims, zlib=True, complevel=4, fill_value=nan_value)
-
-        value.standard_name = value_standard_name
-        value.long_name = value_long_name
-        value.units = value_unit
-        value.valid_min = int(value_min / scale_factor)
-        if value_max != nan_value:
-            value.valid_max = int(value_max / scale_factor)
-        value.scale_factor = scale_factor
-        value.add_offset = add_offset
-        value.missing_value = nan_value
-        value.set_auto_maskandscale(True)
-
-        if proj is not None:
-            if 'grid_mapping_name' in metadata_ncdf[selected_proj_key]:
-                value.grid_mapping = proj.grid_mapping_name
-            if 'spatial_ref' in metadata_ncdf[selected_proj_key]:
-                value.esri_pe_string = proj.spatial_ref
-
-        # Fill variables
-        try:
-            int_lons = settings.binding['internal.lons']
-            int_lats = settings.binding['internal.lats']
-            x1 = Decimal(__DECIMAL_FORMAT.format(int_lons[0]))
-            x2 = Decimal(__DECIMAL_FORMAT.format(int_lons[1]))
-            y1 = Decimal(__DECIMAL_FORMAT.format(int_lats[0]))
-            y2 = Decimal(__DECIMAL_FORMAT.format(int_lats[1]))
-            cellSizeX = abs(x2 - x1)
-            cellSizeY = abs(y2 - y1)
-            lats = coordinates_range_from_array(int_lats)
-            lons = coordinates_range_from_array(int_lons)
-        except:
-            cellSize = Decimal(__DECIMAL_FORMAT.format(pcraster.clone().cellSize()))
-            half_cell = cellSize * Decimal(0.5)
-            xl = Decimal(__DECIMAL_FORMAT.format(pcraster.clone().west())) + half_cell
-            xr = xl + ncols * cellSize - half_cell
-            yu = Decimal(__DECIMAL_FORMAT.format(pcraster.clone().north())) - half_cell
-            yd = yu - nrows * cellSize + half_cell
-            lats = coordinates_range(yu, nrows, -cellSize)
-            lons = coordinates_range(xl, ncols, cellSize)
-        nf1.variables[spatial_dims[1]][:] = lats
-        nf1.variables[spatial_dims[0]][:] = lons
-
-        if 'pr' in metadata_ncdf and 'esri_pe_string' in metadata_ncdf['pr']:
-            value.esri_pe_string = metadata_ncdf['pr']['esri_pe_string']
-
+        nf1 = create_new_netcdf(settings, prefix, netfile, ncols, nrows, time_variable, start_date,
+                                value_standard_name, value_long_name, value_unit, data_type,
+                                scale_factor, add_offset, value_min, value_max,
+                                output6hourly, flag_time, nan_value)
     else:
         nf1 = Dataset(netfile, 'a')
 
