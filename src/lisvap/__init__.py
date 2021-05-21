721e26a384e52aed6885cc27d4832a72fc6e110e
jrgg <goncalo.ramos-gomes@ext.ec.europa.eu>
2021-05-21 13:22:49 +0200

    Correct versioning for pip

--- src/lisvap/__init__.py
+++ src/lisvap/__init__.py
@@ -1,4 +1,4 @@
-version = tuple(map(int, open('VERSION').read().strip().split('.')))
+version = version = (1, 0, 2)
 __authors__ = "Peter Burek, Johan van der Knijff, Ad de Roo"
 __version__ = '.'.join(list(map(str, version)))
 __date__ = "27/03/2020"
