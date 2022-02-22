30e55049ad2a2d075a5422bff58c35abd9589e1c
Carlo Russo <carlo.russo@email.it>
2022-02-22 15:18:00 +0100

    Docker image build update

--- build_docker.sh
+++ build_docker.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-docker build -t efas/lisvap:latest .
-docker tag efas/lisvap:latest index.docker.io/efas/lisvap:latest
-docker tag efas/lisvap:latest d-registry.jrc.it:5000/e1-smfr/lisvap:latest
+docker build -t jrce1/lisvap:latest .
+docker tag jrce1/lisvap:latest index.docker.io/jrce1/lisvap:latest
+docker tag jrce1/lisvap:latest d-registry.jrc.it:5000/e1-smfr/lisvap:latest
 
 echo
 echo
-echo "Lisvap Docker image was built. To push to efas docker repository, first do 'docker login' and then:"
-echo "'docker push efas/lisvap:latest'"
+echo "Lisvap Docker image was built. To push to jrce1 docker repository, first do 'docker login' and then:"
+echo "'docker push jrce1/lisvap:latest'"
 echo
 echo
