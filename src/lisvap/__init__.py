23f73c218c09c1f4fff2a9d0409ab54b45f23de8
jrgg <goncalo.ramos-gomes@ext.ec.europa.eu>
2022-06-28 14:07:57 +0200

    Update version number

--- src/lisvap/__init__.py
+++ src/lisvap/__init__.py
@@ -1,7 +1,7 @@
-version = version = (1, 1, 0)
+version = version = (2, 0, 0)
 __authors__ = "Peter Burek, Johan van der Knijff, Ad de Roo"
 __version__ = '.'.join(list(map(str, version)))
-__date__ = "20/06/2022"
+__date__ = "30/06/2022"
 __copyright__ = "Copyright 2020, Lisflood Open Source"
 __maintainers__ = "Goncalo Gomes, Domenico Nappo, Valerio Lorini, Lorenzo Mentaschi, Lorenzo Alfieri"
 __status__ = "Operation"
