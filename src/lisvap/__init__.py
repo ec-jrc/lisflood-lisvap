ef1f4a8c36f3e4a831dbb0394c8ea9e56a02d981
gnrgomes <ggomes76@gmail.com>
2023-10-05 09:23:03 +0200

    Update version

--- src/lisvap/__init__.py
+++ src/lisvap/__init__.py
@@ -1,7 +1,7 @@
-version = version = (1, 2, 7)
+version = version = (1, 2, 8)
 __authors__ = "Peter Burek, Johan van der Knijff, Ad de Roo"
 __version__ = '.'.join(list(map(str, version)))
-__date__ = "16/05/2023"
+__date__ = "05/10/2023"
 __copyright__ = "Copyright 2020, Lisflood Open Source"
 __maintainers__ = "Goncalo Gomes, Domenico Nappo, Valerio Lorini, Lorenzo Mentaschi, Lorenzo Alfieri"
 __status__ = "Operation"
