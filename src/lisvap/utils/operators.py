Correct RG readmeteo and arcsin and unit conversion

src/lisvap/utils/operators.py
@@ -2,16 +2,14 @@ import numpy as np
 from ..utils import MaskMapMetadata
 
 
-def round_decimals(expression):
-    return np.round(expression, decimals=16)
-
-
 sqr = np.square
 exp = np.exp
 maximum = np.maximum
 ifthenelse = np.where
 abs = np.abs
 
+def round_decimals(expression):
+    return np.round(expression, decimals=16)
 
 def sin(angle_degrees):
     return np.sin(np.deg2rad(round_decimals(angle_degrees)))
@@ -25,7 +23,12 @@ def tan(angle_degrees):
 def asin(x):
     clip_x = x.copy()
     clip_x[(clip_x < -1) | (clip_x > 1)] = np.nan
-    angle_degrees = round_decimals(np.rad2deg(np.arcsin(clip_x)))
+    angle_rads = np.arcsin(clip_x)
+    # To reproduce the same behaviour as pcraster asin function
+    # we need to return positive angles therefore for negative angle
+    # in radians we need to rotate in the positive radian direction 
+    angle_rads[angle_rads < 0.0] += 2 * np.pi
+    angle_degrees = round_decimals(np.rad2deg(angle_rads))
     return angle_degrees
 
 def sqrt(x):
@@ -43,21 +46,22 @@ def scalar(expression):
     if isinstance(expression, (int, float,
                                np.float16, np.float32, np.float64,
                                np.int16, np.int32, np.int64)):
-        scalar_array = np.ma.masked_array(np.empty(maskmap.shape), mask=maskmap.mask)
-        scalar_array.fill(expression)
+        scalar_array = np.ma.masked_array(np.full(shape=maskmap.shape, fill_value=expression), mask=maskmap.mask, fill_value=np.nan)
+    elif isinstance(expression, np.ma.MaskedArray):
+        scalar_array = expression.copy()
     else:
-        scalar_array = np.ma.masked_where(~np.ma.getmask(maskmap), expression)
+        # scalar_array = np.ma.masked_where(~np.ma.getmask(maskmap), expression)
+        scalar_array = np.ma.masked_array(expression.flatten().reshape(maskmap.shape), mask=maskmap.mask, fill_value=np.nan)
     return scalar_array
 
 def cover(input_map, mv=0.0):
     """
     This operator is used to cover missing values on a map.
     """
-    if isinstance(input_map, np.ndarray):
-        nparray = input_map.copy()
-        nparray[np.isnan(nparray)] = mv
-        return nparray
-    return np.where(input_map.mask, mv, input_map)
+    npmasked = scalar(input_map)
+    nparray = np.ma.getdata(npmasked.filled(), subok=False)
+    nparray[np.isnan(nparray)] = mv
+    return np.ma.masked_array(nparray, mask=npmasked.mask, fill_value=np.nan)
 
 def defined(input_map):
     """

src/lisvap/utils/readers.py
@@ -133,9 +133,9 @@ def loadmap(name):
             mv = 0
         # mapnp[np.isnan(mapnp)] = mv
         maskmap = MaskMapMetadata.instance().get_maskmap()
+        mapnp[np.isnan(mapnp)] = np.nan
         mapnp[maskmap.mask] = np.nan
-        res = mapnp
-        # mapnp[np.isnan(mapnp)] = (nan_value - add_offset) * scale_factor
+        res = np.ma.masked_array(mapnp.flatten().reshape(maskmap.shape), mask=maskmap.mask, fill_value=np.nan)
 
         # if the map is a ldd
         if value.split('.')[0][-3:] == 'ldd':
@@ -288,7 +288,13 @@ def readnetcdf(name, timestep, timestampflag='closest', averageyearflag=False, v
     settings = LisSettings.instance()
     if settings.flags['checkfiles']:
         checkmap(timename, filename, mapnp, True, 1)
-    return mapnp
+    maskmap = MaskMapMetadata.instance().get_maskmap()
+    mapnp[np.isnan(mapnp)] = np.nan
+    mapnp[maskmap.mask] = np.nan
+    conversion_factor = settings.get_unit_conversion(variable_binding)
+    if conversion_factor is not None and conversion_factor != 1:
+        mapnp = mapnp * conversion_factor
+    return np.ma.masked_array(mapnp.flatten().reshape(maskmap.shape), mask=maskmap.mask, fill_value=np.nan)
 
 
 def netcdf_step(averageyearflag, nf1, timestampflag, timestep, splitIO=False):

src/lisvap/utils/writers.py
@@ -296,7 +296,7 @@ def writenet(current_output_index, inputmap, netcdf_output_file, current_timeste
         nf1 = Dataset(netfile, 'a')
 
     slices = cutmap.slices
-    mapnp = inputmap[slices[0], slices[1]]
+    mapnp = inputmap.filled(np.nan)[slices[0], slices[1]]
     # Pack NAN values into short
     mapnp[np.isnan(mapnp)] = (nan_value - add_offset) * scale_factor
     if flag_time: